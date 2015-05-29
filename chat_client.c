17a945110eb1aa42f0bb4532f96eaa7ac2b3b928
Your Name <you@example.com>
2015-05-29 18:36:30 +0700

    Add non-blocking I/O function

--- chat_client.c
+++ chat_client.c
@@ -9,11 +9,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void str_cli_non_block(int socket_fd) {
+    char data_received[MAXLINE+1], data_send[MAXLINE+1];
+    int data_received_length, data_send_length;
 
-int main(int argc, char **argv) {
-    // socket file descriptor
-    int socket_fd;
+    fcntl(socket_fd, F_SETFL, O_NONBLOCK);
+    fcntl(0, F_SETFL, O_NONBLOCK);
 
+    for (;;) {
+        if (fgets(data_send, MAXLINE, stdin) != NULL) {
+            if (strcmp(data_send, "quit") == 0) exit(0);
+            data_send_length = strlen(data_send);
+            writen(socket_fd, data_send, data_send_length);
+        }        
+
+        data_received_length = read(socket_fd, data_received, MAXLINE);
+        if (data_received_length> 0) {
+            data_received[data_received_length] = 0; // mark end of string
+            if (fputs(data_received, stdout) == EOF) {
+                err_sys("fputs error");
+            }
+        }
+    }
+        
+}
+
+void str_cli_multiplexing(int socket_fd) {
     // fd set
     fd_set master_read, readfds;
     int fd_max;
@@ -21,6 +42,44 @@ int main(int argc, char **argv) {
     // data received from server
     char data_received[MAXLINE+1], data_send[MAXLINE+1];
     int data_received_length, data_send_length;
+
+    FD_ZERO(&master_read);
+    FD_SET(0, &master_read);
+    FD_SET(socket_fd, &master_read);
+    fd_max = socket_fd;
+
+    for (;;) {
+        // copy master to temporary fd set
+        readfds = master_read;
+
+        if (select(fd_max+1, &readfds, NULL, NULL, NULL) == -1) {
+            err_sys("client: select");
+        }
+
+        if (FD_ISSET(0, &readfds)) {
+            if (fgets(data_send, MAXLINE, stdin) != NULL) {
+            if (strcmp(data_send, "quit") == 0) exit(0);
+                data_send_length = strlen(data_send);
+                writen(socket_fd, data_send, data_send_length);
+            }
+        } else if (FD_ISSET(socket_fd, &readfds)) {
+            data_received_length = read(socket_fd, data_received, MAXLINE);
+            if (data_received_length> 0) {
+            data_received[data_received_length] = 0; // mark end of string
+                if (fputs(data_received, stdout) == EOF) {
+                    err_sys("fputs error");
+                }
+            }
+        }
+
+
+    }
+
+}
+
+int main(int argc, char **argv) {
+    // socket file descriptor
+    int socket_fd;
 
     // server address
     struct sockaddr_in server_address;
@@ -31,8 +90,8 @@ int main(int argc, char **argv) {
     char *server_port = "1337";
 
     if (argc > 1) {
-	server_ip = argv[1];
-	if (argc > 2) {
+    	server_ip = argv[1];
+    	if (argc > 2) {
     	    server_port = argv[2];
     	}
     }
@@ -51,44 +110,12 @@ int main(int argc, char **argv) {
     if (socket_fd < 0) {
 	err_sys("socket error");
     }
-    FD_ZERO(&master_read);
-    FD_SET(0, &master_read);
-    FD_SET(socket_fd, &master_read);
-    fd_max = socket_fd;
+    
 
     if (connect(socket_fd, (SA *)&server_address, sizeof server_address)< 0) {
     	err_sys("connect error");
     }
-    for (;;) {
-	// copy master to temporary fd set
-	readfds = master_read;
-
-	if (select(fd_max+1, &readfds, NULL, NULL, NULL) == -1) {
-	    err_sys("client: select");
-	}
-
-	if (FD_ISSET(0, &readfds)) {
-	    if (fgets(data_send, MAXLINE, stdin) != NULL) {
-		if (strcmp(data_send, "quit") == 0) exit(0);
-	    	data_send_length = strlen(data_send);
-	    	writen(socket_fd, data_send, data_send_length);
-	    }
-	} else if (FD_ISSET(socket_fd, &readfds)) {
-	    data_received_length = read(socket_fd, data_received, MAXLINE);
-	    if (data_received_length> 0) {
-		data_received[data_received_length] = 0; // mark end of string
-	    	if (fputs(data_received, stdout) == EOF) {
-	    	    err_sys("fputs error");
-	    	}
-	    }
-	}
-
-
-    }
-
-    if (data_received_length < 0) {
-    	err_sys("read error");
-    }
+    str_cli_non_block(socket_fd);
 
     exit(0);
 }
--- chat_server.c
+++ chat_server.c
@@ -14,31 +14,225 @@ void broadcast_message(char *message, int message_length, int fd_max, fd_set* fd
     message[message_length] = 0;
     int i;
     for (i=0; i<=fd_max; i++) {
-	if (FD_ISSET(i, fds)) {
-	    if (i==listener || i == itself_fd) continue;
-	    if (writen(i, message, message_length) == -1) {
-		perror("server: written");
-	    }
-	}
+		if (FD_ISSET(i, fds)) {
+		    if (i==listener || i == itself_fd) continue;
+		    if (write(i, message, message_length) == -1) {
+				perror("server: written");
+		    }
+		}
     }
 }
 
-int main(int argc, char **argv) {
-    char *port = "1337";
-    struct sockaddr_in server_address, client_address;
-    int server_fd, client_fd, fd_max;
+void handle_server_non_block(int server_fd) {
+	int i;
+	char *str_tmp;
+	struct sockaddr_in client_address;
+    int client_fd, fd_max;
     socklen_t client_address_length;
     char client_ip_address[INET6_ADDRSTRLEN];
     char client_message[MAXLINE+1], server_message[MAXLINE+1];
-    char *str_tmp;
-    int i;
+
+    // file descriptor set
+    fd_set master; // master fdset
+
+     // initially, maximum fd is the server_fd
+    fd_max = server_fd;
+
+    // set the server_fd and stdin into master fd set
+    FD_SET(server_fd, &master);
+    FD_SET(0, &master);
+
+    fcntl(0, F_SETFL, O_NONBLOCK);
+    fcntl(server_fd, F_SETFL, O_NONBLOCK);
+
+    // new connection's coming
+		client_address_length = sizeof client_address;
+	    bzero(&client_address, client_address_length);
+
+    for (;;) {	    
+	    // get client fd
+		client_fd = accept(server_fd, (SA *)&client_address, &client_address_length);
+
+		if (client_fd == -1) {
+	    	if (errno != EWOULDBLOCK)
+				perror("server: accept");
+	    } else {
+			// add client_fd into master fd set
+	    	FD_SET(client_fd, &master);
+
+	    	// update fd_max if needed
+	    	if (client_fd > fd_max) {
+	    	    fd_max = client_fd;
+	    	}
+
+	    	fcntl(client_fd, F_SETFL, O_NONBLOCK);
+
+	    	// get the client_ip_address
+	    	inet_ntop(client_address.sin_family, &(client_address.sin_addr), client_ip_address, sizeof client_ip_address);
+	    	printf("*** We've got a new connection from %s ***\n", client_ip_address);
+	    	
+		    client_address_length = sizeof client_address;
+		    bzero(&client_address, client_address_length);
+	    }
+	
+
+		for (i=0; i<=fd_max; i++) {
+		    // check active fds
+		    if (FD_ISSET(i, &master)) {
+				if (i == server_fd) {
+				    continue;
+				} else if (i == 0) {
+				    // get input from stdin
+				    if (fgets(server_message, MAXLINE, stdin) != NULL) {
+						str_tmp = strdup(server_message);
+						strcpy(server_message, "Server: ");
+						strcat(server_message, str_tmp);
+						free(str_tmp);
+						broadcast_message(server_message, strlen(server_message), fd_max, &master, i, server_fd);
+				    }
+				} else {
+				    // handle input from client
+				    int n;
+				    n = read(i, client_message, MAXLINE);
+				    if (n<=0) {
+						if (n == 0) {
+						    // connection closed
+						    printf("*** Connection from socket %d hung up ***\n", i);
+						    close(i);
+						    FD_CLR(i, &master);
+						} else {
+							if (errno != EWOULDBLOCK) {
+							    // connection error
+							    perror("server: read");
+							    close(i);
+							    FD_CLR(i, &master);
+							}
+						}
+
+
+				    } else {
+					    client_message[n] = 0;
+
+					    str_tmp = strdup(client_message);
+					    strcpy(client_message, "");
+					    snprintf(client_message, MAXLINE, "Socket %d: ", i);
+					    strcat(client_message, str_tmp);
+					    free(str_tmp);
+
+					    broadcast_message(client_message, strlen(client_message), fd_max, &master, i, server_fd);
+					}
+				}
+		    }
+		}
+    }
+}
+
+void handle_server_multiplexing(int server_fd) {
+	int i;
+	char *str_tmp;
+	struct sockaddr_in client_address;
+    int client_fd, fd_max;
+    socklen_t client_address_length;
+    char client_ip_address[INET6_ADDRSTRLEN];
+    char client_message[MAXLINE+1], server_message[MAXLINE+1];
 
     // file descriptor set
     fd_set master; // master fdset
     fd_set readfds; // temporary fdset used in select functions
 
+     // initially, maximum fd is the server_fd
+    fd_max = server_fd;
+
+    // set the server_fd and stdin into master fd set
+    FD_SET(server_fd, &master);
+    FD_SET(0, &master);
+
+    for (;;) {
+
+		if (select(fd_max+1, &readfds, NULL, NULL, NULL) == -1) {
+		    err_sys("server: select");
+		}
+
+		if (FD_ISSET(server_fd, &readfds)) {
+		    // new connection's coming
+		    client_address_length = sizeof client_address;
+		    bzero(&client_address, client_address_length);
+
+		    // get client fd
+		    client_fd = Accept(server_fd, (SA *)&client_address, &client_address_length);
+
+		    if (client_fd == -1) {
+		    	perror("server: accept");
+		    } else {
+			// add client_fd into master fd set
+		    	FD_SET(client_fd, &master);
+
+		    	// update fd_max if needed
+		    	if (client_fd > fd_max) {
+		    	    fd_max = client_fd;
+		    	}
+
+		    	// get the client_ip_address
+		    	inet_ntop(client_address.sin_family, &(client_address.sin_addr), client_ip_address, sizeof client_ip_address);
+		    	printf("*** We've got a new connection from %s ***\n", client_ip_address);
+		    }
+		}
+
+		for (i=0; i<=fd_max; i++) {
+		    // check active fds
+		    if (FD_ISSET(i, &readfds)) {
+				if (i == server_fd) {
+				    continue;
+				} else if (i == 0) {
+				    // get input from stdin
+				    if (fgets(server_message, MAXLINE, stdin) != NULL) {
+					str_tmp = strdup(server_message);
+					strcpy(server_message, "Server: ");
+					strcat(server_message, str_tmp);
+					free(str_tmp);
+					broadcast_message(server_message, strlen(server_message), fd_max, &master, i, server_fd);
+				    }
+				} else {
+				    // handle input from client
+				    int n;
+				    n = read(i, client_message, MAXLINE);
+				    if (n<=0) {
+					if (n == 0) {
+					    // connection closed
+					    printf("*** Connection from socket %d hung up ***\n", i);
+					} else {
+					    // connection error
+					    perror("server: read");
+					}
+
+					close(i);
+					FD_CLR(i, &master);
+					continue;
+				    }
+
+				    client_message[n] = 0;
+
+				    str_tmp = strdup(client_message);
+				    strcpy(client_message, "");
+				    snprintf(client_message, MAXLINE, "Socket %d: ", i);
+				    strcat(client_message, str_tmp);
+				    free(str_tmp);
+
+				    broadcast_message(client_message, strlen(client_message), fd_max, &master, i, server_fd);
+				}
+		    }
+		}
+    }
+
+}
+
+int main(int argc, char **argv) {
+    char *port = "1337";
+    struct sockaddr_in server_address;
+    int server_fd;
+
     if (argc > 1) {
-	port = argv[1];
+		port = argv[1];
     }
 
     // generate server_address
@@ -49,97 +243,14 @@ int main(int argc, char **argv) {
 
     // create server socket
     server_fd = socket(server_address.sin_family, SOCK_STREAM, 0);
-
-    // initially, maximum fd is the server_fd
-    fd_max = server_fd;
-
-    // set the server_fd and stdin into master fd set
-    FD_SET(server_fd, &master);
-    FD_SET(0, &master);
-
+   
     Bind(server_fd, (SA *)&server_address, sizeof server_address);
 
     Listen(server_fd, LISTENQ);
 
     printf("Server's starting listen on port %s\n\n", port);
 
-    for (;;) {
-	// copy master to temporary fd set
-	readfds = master;
+    handle_server_non_block(server_fd);
 
-	if (select(fd_max+1, &readfds, NULL, NULL, NULL) == -1) {
-	    err_sys("server: select");
-	}
-
-	if (FD_ISSET(server_fd, &readfds)) {
-	    // new connection's coming
-	    client_address_length = sizeof client_address;
-	    bzero(&client_address, client_address_length);
-
-	    // get client fd
-	    client_fd = Accept(server_fd, (SA *)&client_address, &client_address_length);
-
-	    if (client_fd == -1) {
-		perror("server: accept");
-	    } else {
-		// add client_fd into master fd set
-	    	FD_SET(client_fd, &master);
-
-	    	// update fd_max if needed
-	    	if (client_fd > fd_max) {
-	    	    fd_max = client_fd;
-	    	}
-
-	    	// get the client_ip_address
-	    	inet_ntop(client_address.sin_family, &(client_address.sin_addr), client_ip_address, sizeof client_ip_address);
-	    	printf("*** We've got a new connection from %s ***\n", client_ip_address);
-	    }
-	}
-
-	for (i=0; i<=fd_max; i++) {
-	    // check active fds
-	    if (FD_ISSET(i, &readfds)) {
-		if (i == server_fd) {
-		    continue;
-		} else if (i == 0) {
-		    // get input from stdin
-		    if (fgets(server_message, MAXLINE, stdin) != NULL) {
-			str_tmp = strdup(server_message);
-			strcpy(server_message, "Server: ");
-			strcat(server_message, str_tmp);
-			free(str_tmp);
-			broadcast_message(server_message, strlen(server_message), fd_max, &master, i, server_fd);
-		    }
-		} else {
-		    // handle input from client
-		    int n;
-		    n = read(i, client_message, MAXLINE);
-		    if (n<=0) {
-			if (n == 0) {
-			    // connection closed
-			    printf("*** Connection from socket %d hung up ***\n", i);
-			} else {
-			    // connection error
-			    perror("server: read");
-			}
-
-			close(i);
-			FD_CLR(i, &master);
-			continue;
-		    }
-
-		    client_message[n] = 0;
-
-		    str_tmp = strdup(client_message);
-		    strcpy(client_message, "");
-		    snprintf(client_message, MAXLINE, "Socket %d: ", i);
-		    strcat(client_message, str_tmp);
-		    free(str_tmp);
-
-		    broadcast_message(client_message, strlen(client_message), fd_max, &master, i, server_fd);
-		}
-	    }
-	}
-    }
-
+    return 0;
 }
